25b1cfc594c6deccb4144c176e9853eacac42e34
alsotang <alsotang@gmail.com>
2021-03-10 15:18:43 +0800

    todo

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # 用例
+
 用例表格：https://docs.qq.com/sheet/DRXhuQVRYWk95a3hP?tab=h17h2i
-
-## 帧率
-
-
-## 内存
-
-### 1. DOM同级数量：
-- 100个 无样式div：https://{cdn-go}/memory/1.html
---
+++ src/cases/memory/100_plain_div.js
@@ -0,0 +1,17 @@
+const benchmark = {
+  html() {
+    return ```
+    ```
+  },
+  script() {
+
+  },
+  name() {
+
+  },
+  description() {
+
+  },
+}
+
+export default benchmark;
