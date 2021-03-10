407875e25d473c5ee17c813620f7e46cfff0dc6a
alsotang <alsotang@gmail.com>
2021-03-10 16:16:24 +0800

    feat: pfbs转换

---
+++ bin/build.js
@@ -0,0 +1,27 @@
+const glob = require("glob")
+const mkdirp = require('mkdirp')
+const pathlib = require('path')
+const child_process = require('child_process')
+const rimraf = require('rimraf')
+
+
+const projectRoot = pathlib.join(__dirname, '..')
+const distDir = pathlib.join(projectRoot, 'dist')
+rimraf.sync(distDir);
+
+glob(pathlib.join(projectRoot, "cases/**/*.{js,html}"), {}, function (er, files) {
+  files.forEach(filename => {
+    const ext = pathlib.extname(filename);
+    const isJs = ext === '.js'
+    const jsArgs = isJs ? '--js' : '';
+
+    const relativePath = pathlib.relative(projectRoot, filename)
+    const target = pathlib.join(distDir, relativePath)
+    const targetDir  = pathlib.dirname(target)
+    mkdirp.sync(targetDir)
+
+    const pfbsPath = pathlib.join(projectRoot, 'bin', 'pfbs')
+
+    child_process.exec(`${pfbsPath} ${jsArgs} --src ${filename} --out ${target}`);
+  })
+})
