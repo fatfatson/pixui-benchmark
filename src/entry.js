ec927efea8a9af0ddfdfed6d789a31f5e11f6164
alsotang <alsotang@gmail.com>
2021-03-11 18:53:48 +0800

    feat: autoGc

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,17 @@
 import {benchmark} from '{{{benchmark_case}}}'
 
+const autoGc = (duration = 160) => {
+  if (globalThis.std?.gc) {
+    // 避免浏览器报错
+    setTimeout(() => {
+      std.gc();
+      autoGc(duration);
+    }, duration);
+  }
+};
+
+autoGc();
+
 window.moveTo(100, 100);
 window.resizeTo(400, 400);
 
