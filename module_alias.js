78ac75982d49c31883f6c3cb416dd222739beee5
gisonyeung <gisonyeung@gmail.com>
2021-07-19 16:20:36 +0800

    style: eslint

--- module_alias.js
+++ module_alias.js
@@ -1,7 +1,7 @@
-const moduleAlias = require('module-alias')
-const pathlib = require('path')
+const moduleAlias = require('module-alias');
+const pathlib = require('path');
 
 // Or multiple aliases
 moduleAlias.addAliases({
   '@': pathlib.join(__dirname, 'src'),
-})
+});
