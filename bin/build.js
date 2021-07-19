443b59b46edff3efa383958fb6e326c2d1f1b094
gisonyeung <gisonyeung@gmail.com>
2021-07-19 16:15:15 +0800

    style: eslint

--- bin/build.js
+++ bin/build.js
@@ -1,47 +1,46 @@
-require('../module_alias')
+require('../module_alias');
 
-const pathlib = require('path')
-const glob = require('glob')
-const mkdirp = require('mkdirp')
-const rimraf = require('rimraf')
-const fs = require('fs')
-const { compileCaseToBundleHtml, pfbsCompile, projectRoot } = require('@/lib/compile')
+const pathlib = require('path');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const fs = require('fs');
+const { compileCaseToBundleHtml, pfbsCompile, projectRoot } = require('@/lib/compile');
 
 
-const srcDir = pathlib.join(projectRoot, 'src')
-const distDir = pathlib.join(projectRoot, 'dist')
-rimraf.sync(distDir)
-
+const srcDir = pathlib.join(projectRoot, 'src');
+const distDir = pathlib.join(projectRoot, 'dist');
+rimraf.sync(distDir);
 
 
 async function main() {
   // 所有用例
-  const benchmarkCaseFiles = glob.sync(`${projectRoot}/src/cases/**/*.case.js`)
+  const benchmarkCaseFiles = glob.sync(`${projectRoot}/src/cases/**/*.case.js`);
 
   for (const benchmarkCaseFile of benchmarkCaseFiles) {
-    const html = compileCaseToBundleHtml(benchmarkCaseFile)
+    const html = compileCaseToBundleHtml(benchmarkCaseFile);
 
-    const relativePath = pathlib.relative(srcDir, benchmarkCaseFile)
+    const relativePath = pathlib.relative(srcDir, benchmarkCaseFile);
     const htmlTarget = pathlib.join(distDir, relativePath).replace(/\.case\.js$/, '.html');
     const htmlTargetDir = pathlib.dirname(htmlTarget);
-    mkdirp.sync(htmlTargetDir)
+    mkdirp.sync(htmlTargetDir);
 
     fs.writeFileSync(htmlTarget.replace(/\.html$/, '.web.html'), html);
 
-    const pfbsHtml_2_0 = await pfbsCompile(html)
+    const pfbsHtml_2_0 = await pfbsCompile(html);
 
-    fs.writeFileSync(htmlTarget, pfbsHtml_2_0, 'binary')
+    fs.writeFileSync(htmlTarget, pfbsHtml_2_0, 'binary');
 
-    const pfbsHtml_3_0 = await pfbsCompile(html, {version: '3.0'})
+    const pfbsHtml_3_0 = await pfbsCompile(html, { version: '3.0' });
 
-    fs.writeFileSync(htmlTarget.replace(/\.html$/, '.3_0.html'), pfbsHtml_3_0, 'binary')
+    fs.writeFileSync(htmlTarget.replace(/\.html$/, '.3_0.html'), pfbsHtml_3_0, 'binary');
 
-    const pfbsHtml_4_0 = await pfbsCompile(html, {version: '4.0'})
+    const pfbsHtml_4_0 = await pfbsCompile(html, { version: '4.0' });
 
-    fs.writeFileSync(htmlTarget.replace(/\.html$/, '.4_0.html'), pfbsHtml_4_0, 'binary')
+    fs.writeFileSync(htmlTarget.replace(/\.html$/, '.4_0.html'), pfbsHtml_4_0, 'binary');
   }
 }
 
-main()
+main();
 
 
--- bin/dev_server.js
+++ bin/dev_server.js
@@ -1,34 +1,34 @@
-require('../module_alias')
+require('../module_alias');
 
-const express = require('express')
-const fs = require('fs')
-const pahtlib = require('path')
-const { compileCaseToBundleHtml, pfbsCompile } = require('@/lib/compile')
-const os = require('os')
+const express = require('express');
+const fs = require('fs');
+const pahtlib = require('path');
+const { compileCaseToBundleHtml, pfbsCompile } = require('@/lib/compile');
+const os = require('os');
 
 
 const app = express();
 
 // 免得报错
-app.get('/favicon.ico', function (req, res) {
+app.get('/favicon.ico', (req, res) => {
   res.send();
-})
+});
 
-//拦截输出，当agent为PixUI时，转换为fbs二进制数据再返回
+// 拦截输出，当agent为PixUI时，转换为fbs二进制数据再返回
 app.get('*', async (req, res, next) => {
-  console.log(`request ${req.url}`)
+  console.log(`request ${req.url}`);
 
-  const reqPathname = req.path
+  const reqPathname = req.path;
   const ua = req.get('User-Agent');
 
-  const ext = pahtlib.extname(reqPathname)
+  const ext = pahtlib.extname(reqPathname);
   const extToContentType = {
     '.html': 'text/html',
     '.js': 'application/javascript',
-    '.css': 'text/css'
-  }
+    '.css': 'text/css',
+  };
 
-  res.set('content-type', extToContentType[ext] || 'text/plain')
+  res.set('content-type', extToContentType[ext] || 'text/plain');
 
 
   const isPixUI = /PixUI/.test(ua);
@@ -38,7 +38,7 @@ app.get('*', async (req, res, next) => {
 
 
   if (!fs.existsSync(relatedJs) || fs.statSync(relatedJs).isDirectory()) {
-    return res.send('not found')
+    return res.send('not found');
   }
 
   const htmlContent = compileCaseToBundleHtml(relatedJs);
@@ -46,12 +46,12 @@ app.get('*', async (req, res, next) => {
     let pfbsVersion = '';
     if (pixuiVersion) {
       const minorVersion = parseInt(pixuiVersion[2]);
-      pfbsVersion = `0.${minorVersion}`
+      pfbsVersion = `0.${minorVersion}`;
     }
     const fbsContent = await pfbsCompile(htmlContent, { version: pfbsVersion });
-    res.send(fbsContent)
+    res.send(fbsContent);
   } else {
-    res.send(htmlContent)
+    res.send(htmlContent);
   }
 });
 
@@ -59,25 +59,25 @@ app.get('*', async (req, res, next) => {
 function getAllAddress() {
   const ifaces = os.networkInterfaces();
 
-  const addrs = []
+  const addrs = [];
   if (ifaces) {
-    Object.keys(ifaces).forEach(function (dev) {
-      ifaces[dev].forEach(function (details) {
+    Object.keys(ifaces).forEach((dev) => {
+      ifaces[dev].forEach((details) => {
         if (details.family === 'IPv4') {
-          addrs.push(details.address)
+          addrs.push(details.address);
           // console.info(('  ' + protocol + details.address + ':' + colors.green(port.toString())));
         }
       });
     });
   }
 
-  return addrs
+  return addrs;
 }
 
-const appPort = 8083
+const appPort = 8083;
 app.listen(appPort, () => {
-  console.log(`app is listening at:`)
-  getAllAddress().forEach(address => {
-    console.log(`http://${address}:${appPort}/`)
-  })
-})
+  console.log('app is listening at:');
+  getAllAddress().forEach((address) => {
+    console.log(`http://${address}:${appPort}/`);
+  });
+});
